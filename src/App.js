7ed36c833c4b6fa20544e81752967d69417290c2
José Ramón Luna Calasich <aguilas9927@hotmail.com>
2022-09-24 16:40:18 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { app } from "./fb";
+import './App.css';
 import Rutas from './rutas/Rutas';
-import Logueo from "./Logueo"; 
-
 
 
 function App() {
-  const [usuario, setUsuario] = React.useState(null);
-  useEffect(() => {
-    app.auth().onAuthStateChanged((usuarioFirebase) => {
-      console.log("ya tienes sesión iniciada con:", usuarioFirebase);
-      setUsuario(usuarioFirebase);
-    });
-  }, []);
-
-  return <>{usuario ? <Rutas/> : <Logueo setUsuario={setUsuario} />}</>;
+  return (
+    <div className="App">
+    <Rutas/>
+    </div>
+  );
 }
-
 export default App;
