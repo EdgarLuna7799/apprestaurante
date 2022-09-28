21fe768f52c9f6a63fdf25653261643356363418
José Ramón Luna Calasich <aguilas9927@hotmail.com>
2022-09-28 13:06:41 -0500

    se agrego el front

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React, { useEffect } from "react";
+import { app } from "./fb";
+import Logueo from "./Logueo";
 import './App.css';
 import Rutas from './rutas/Rutas';
 
 
 function App() {
-  return (
-    <div className="App">
-    <Rutas/>
-    </div>
-  );
+  const [usuario, setUsuario] = React.useState(null);
+  useEffect(() => {
+    app.auth().onAuthStateChanged((usuarioFirebase) => {
+      console.log("ya tienes sesión iniciada con:", usuarioFirebase);
+      setUsuario(usuarioFirebase);
+    });
+  }, []);
+
+  return <>{usuario ? <Rutas/> : <Logueo setUsuario={setUsuario} />}</>;
 }
+
 export default App;
---
+++ src/fb.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+
+export const app = firebase.initializeApp({
+    "projectId": "autenticacion-6913b",
+    "appId": "1:383996483849:web:4f27186e9f1af5f9c09bd8",
+    "storageBucket": "autenticacion-6913b.appspot.com",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyAKKc7cjbkIb1UQ1RwXeCsuJRYhyp3dPhM",
+    "authDomain": "autenticacion-6913b.firebaseapp.com",
+    "messagingSenderId": "383996483849"
+  });
+  
